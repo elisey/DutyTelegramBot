58727f15f3f7ab8a03b45ecc50700602ddb87b19
Elisey <elisey.rav@gmail.com>
2017-09-19 23:50:41 +0300

    Add logging

--- connectTeamBot.py
+++ connectTeamBot.py
@@ -7,7 +7,7 @@ import configparser
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+                    level=logging.INFO, filename='connectTeamBot.log')
 
 logger = logging.getLogger(__name__)
 
@@ -20,30 +20,43 @@ def setDutyName(newName):
     file.write(newName.encode("UTF-8"))
 
 def set_duty(bot, update, args):
+    printCommandLog('Set_duty', update.message.from_user)
+
     if len(args) == 1:
         setDutyName(args[0])
         bot.sendMessage(update.message.chat_id, text = "Новое имя установлено")
+        logger.info('New name %s', args[0])
     else:
         bot.sendMessage(update.message.chat_id, text = "Неправильный параметр команды")
 
 def get_duty(bot, update, args):
+    printCommandLog('Get_duty', update.message.from_user)
+
     dutyName = getDutyName()
     bot.sendMessage(update.message.chat_id, text=dutyName)
 
 def start(bot, update):
+    printCommandLog('Start', update.message.from_user)
+
     bot.sendMessage(update.message.chat_id, text='Привет')
 
 def help(bot, update):
+    printCommandLog('Help', update.message.from_user)
     bot.sendMessage(update.message.chat_id, text='Написать "дежурный" или /get_duty чтобы получить имя.')
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
 
 def freeText(bot, update):
+    printCommandLog('Free text', update.message.from_user)
     inputText = update.message.text
     if inputText == "дежурный".decode('utf-8'):
         dutyName = getDutyName()
         bot.sendMessage(update.message.chat_id, text=dutyName)
+
+def printCommandLog(commandName, user):
+    logger.info(commandName + ' command. User %s %s (username %s, id %d)', user.first_name, user.last_name, user.username, user.id)
+
 
 def main():
     config = configparser.ConfigParser()
@@ -73,7 +86,6 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
